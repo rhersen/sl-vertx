20dcee8667e8676dcd0aff040bd599b1b2dcbf0e
Rudolf Hersen <rhersen@gmail.com>
2014-06-05 11:15:26 +0200

    almost all stations

--- src/main/java/com/mycompany/myproject/TrafiklabProxy.java
+++ src/main/java/com/mycompany/myproject/TrafiklabProxy.java
@@ -56,7 +56,12 @@ public class TrafiklabProxy extends Verticle {
 
     private JsonArray getStations() {
         return new JsonArray(
-                asList(9510, 9530, 9531, 9529, 9528, 9527, 9526, 9525, 9524, 9523, 9522, 9521, 9520));
+                asList(
+                        9710, 9711, 9700, 9701, 9702, 9703, 9704, 9325,
+                        9502, 9503, 9504, 9505, 9506, 9507, 9508, 9509,
+                        9510, 9000, 9530, 9531, 9529,
+                        9528, 9527, 9526, 9525, 9524, 9523, 9522, 9521, 9520,
+                        9180, 9732, 9731, 9730, 9729, 9728, 9727, 9726, 9725));
     }
 
     private void handleGetDeparture(HttpServerRequest request, String key) {
