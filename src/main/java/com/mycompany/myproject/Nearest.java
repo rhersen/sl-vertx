1d6430052206ee360acc3991a9ec43dabf2ed257
Rudolf Hersen <rhersen@gmail.com>
2014-07-02 15:08:50 +0200

    oops, null pointer

--- src/main/java/com/mycompany/myproject/Nearest.java
+++ src/main/java/com/mycompany/myproject/Nearest.java
@@ -4,6 +4,7 @@ import org.vertx.java.core.eventbus.Message;
 import org.vertx.java.platform.Verticle;
 
 import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 
 import static java.util.Arrays.stream;
@@ -13,7 +14,10 @@ public class Nearest extends Verticle {
     private NearestImpl nearest;
 
     public void start() {
-        nearest = new NearestImpl(new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("/stoppoints.csv"))).lines());
+        InputStream stream = getClass().getResourceAsStream("/stoppoints.csv");
+        if (stream != null) {
+            nearest = new NearestImpl(new BufferedReader(new InputStreamReader(stream)).lines());
+        }
 
         vertx.eventBus().registerHandler("nearest", (Message<String> m) -> m.reply(nearest.get(stream(m.body().split(",")).mapToDouble(Double::parseDouble).toArray())));
     }
